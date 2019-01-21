671649592a82fcf491219b22c660eb50b10a7508
Massimo Carletti <carletti.massimo@gmail.com>
2019-01-21 14:28:50 +0100

    main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,7 +2,24 @@
 "use strict";
 
 /* jshint esversion: 6 */
+
+/*--------------------------------------------------
+Example
+--------------------------------------------------*/
+function example() {}
+/*--------------------------------------------------
+DOC READY
+--------------------------------------------------*/
+
+
+$(function () {
+  example();
+});
+/*--------------------------------------------------
+WIN LOAD
+--------------------------------------------------*/
+
 window.onload = function () {};
 
 },{}]},{},[1]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzcmMvanMvbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiBqc2hpbnQgZXN2ZXJzaW9uOiA2ICovXHJcblxyXG53aW5kb3cub25sb2FkID0gKCkgPT4ge1xyXG5cclxufTtcclxuIl0sImZpbGUiOiJkb2NzXFxqc1xcbWFpbi5qcyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzcmMvanMvbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiBqc2hpbnQgZXN2ZXJzaW9uOiA2ICovXHJcblxyXG4vKi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tXHJcbkV4YW1wbGVcclxuLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0qL1xyXG5mdW5jdGlvbiBleGFtcGxlKCkge1xyXG5cclxufVxyXG5cclxuLyotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLVxyXG5ET0MgUkVBRFlcclxuLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0qL1xyXG4kKGZ1bmN0aW9uKCkge1xyXG5cclxuXHRleGFtcGxlKCk7XHJcblxyXG59KTtcclxuXHJcbi8qLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS1cclxuV0lOIExPQURcclxuLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0qL1xyXG53aW5kb3cub25sb2FkID0gKCkgPT4ge1xyXG5cclxufTtcclxuIl0sImZpbGUiOiJkb2NzXFxqc1xcbWFpbi5qcyJ9
